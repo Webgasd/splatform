a0dc876b54a377aee7e686dd684ba4ebcfc245f6
yue <1599552627@qq.com>
2021-04-26 18:31:24 +0800

    yue

--- src/pages/supervision/enterprise/index.js
+++ src/pages/supervision/enterprise/index.js
@@ -150,6 +150,43 @@ import {baseUrl} from "../../../axios/commonSrc";
         this.requestList();
     };
 
+    //压缩函数
+    compress = (base64String, w, quality)=>{
+        let getMimeType = function (urlData) {
+            let arr = urlData.split(',');
+            let mime = arr[0].match(/:(.*?);/)[1];
+            // return mime.replace("image/", "");
+            return mime;
+        };
+        let newImage = new Image();
+        let imgWidth, imgHeight;
+ 
+        let promise = new Promise(resolve => newImage.onload = resolve);
+        newImage.src = base64String;
+        return promise.then(() => {
+            imgWidth = newImage.width;
+            imgHeight = newImage.height;
+            let canvas = document.createElement("canvas");
+            let ctx = canvas.getContext("2d");
+            if (Math.max(imgWidth, imgHeight) > w) {
+                if (imgWidth > imgHeight) {
+                    canvas.width = w;
+                    canvas.height = w * imgHeight / imgWidth;
+                } else {
+                    canvas.height = w;
+                    canvas.width = w * imgWidth / imgHeight;
+                }
+            } else {
+                canvas.width = imgWidth;
+                canvas.height = imgHeight;
+            }
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
+            let base64 = canvas.toDataURL(getMimeType(base64String), quality);
+            console.log(base64);
+            return base64;
+        });
+    }
 
     handleSubmit = ()=>{
         let type =this.state.type;
